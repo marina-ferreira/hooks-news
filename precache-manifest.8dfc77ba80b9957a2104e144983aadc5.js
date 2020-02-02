ff5d6b26cb0040128075e55293f3db33145193ac
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-02-02 19:21:18 -0300

    Updates

---
+++ precache-manifest.8dfc77ba80b9957a2104e144983aadc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59115da536599edf327640ec33d1dc2f",
+    "url": "/hooks-news/index.html"
+  },
+  {
+    "revision": "c266cf1454aa0dca6517",
+    "url": "/hooks-news/static/js/2.c39aa307.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/hooks-news/static/js/2.c39aa307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364852205f3e454bd516",
+    "url": "/hooks-news/static/js/main.11d997ff.chunk.js"
+  },
+  {
+    "revision": "1c6d009ee0efb1669222",
+    "url": "/hooks-news/static/js/runtime-main.510c3814.js"
+  }
+]);
