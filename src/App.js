257611ca7de1bb7567405a7603f535f1f3e9f7cc
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-02-02 13:43:29 -0300

    Search on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef, memo } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+  const inputSearchRef = useRef();
 
   const getResults = async () => {
-    const url = 'http://hn.algolia.com/api/v1/search?query=reacthooks';
+    const url = `http://hn.algolia.com/api/v1/search?query=${query}`;
     const response = await axios.get(url);
 
     setResults(response.data.hits);
+    inputSearchRef.current.focus();
   }
 
   const cbGetResults = useCallback(getResults, []);
@@ -17,15 +20,42 @@ function App() {
     cbGetResults();
   }, [cbGetResults]);
 
+  const handleClear = () => {
+    setQuery('');
+    inputSearchRef.current.focus();
+  }
+
   return (
-    <ul>
-      {results.map(result => (
-        result.url && <li key={result.objectID}>
-          <a href={result.url}>{result.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        getResults();
+      }}>
+        <input
+          type='text'
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          ref={inputSearchRef}
+          autofocus='true'
+        />
+
+        <input type='submit' value='Search' />
+        <input type='button' value='Clear' onClick={handleClear} />
+      </form>
+
+      <ArticleList results={results} />
+    </>
   );
 }
+
+const ArticleList = memo(({ results }) => (
+  <ul>
+    {results.map(result => (
+      result.url && <li key={result.objectID}>
+        <a href={result.url}>{result.title}</a>
+      </li>
+    ))}
+  </ul>
+))
 
 export default App;
