a76a43a5e5090b8a3084d471cdce26987b56d6af
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-02-02 12:14:58 -0300

    Lists react hooks articles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    getResults();
+  }, [])
+
+  const getResults = async () => {
+    const url = 'http://hn.algolia.com/api/v1/search?query=reacthooks';
+    const response = await axios.get(url);
+
+    setResults(response.data.hits);
+  }
+
   return (
-    <div className="App">
-      App
-    </div>
+    <ul>
+      {results.map(result => (
+        result.url && <li key={result.objectID}>
+          <a href={result.url}>{result.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 }
 
